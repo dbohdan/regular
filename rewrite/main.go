3e7cd795a01c0e89cc2bd5a8dee27bab92bbc320
D. Bohdan <dbohdan@dbohdan.com>
2024-12-28 10:48:14 +0000

    fix(rw): fix linter errors

--- rewrite/main.go
+++ rewrite/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/fsnotify/fsnotify"
 	"github.com/mna/starstruct"
 	"go.starlark.net/starlark"
+	"go.starlark.net/syntax"
 )
 
 const (
@@ -36,7 +37,13 @@ func jobNameFromPath(path string) string {
 func loadJob(path string) (Job, error) {
 	thread := &starlark.Thread{Name: "job"}
 
-	globals, err := starlark.ExecFile(thread, path, nil, nil)
+	globals, err := starlark.ExecFileOptions(
+		&syntax.FileOptions{},
+		thread,
+		path,
+		nil,
+		nil,
+	)
 	if err != nil {
 		return Job{}, err
 	}
@@ -56,69 +63,19 @@ func loadJob(path string) (Job, error) {
 	return job, nil
 }
 
-type updateJobsResult int
-
-const (
-	jobsNoChanges updateJobsResult = iota
-	jobsAddedNew
-	jobsUpdated
-)
-
-func updateJobs(jobs map[string]Job, jobsMu sync.RWMutex, path string) (updateJobsResult, error) {
-	jobName := jobNameFromPath(path)
-
-	job, err := loadJob(path)
-	if err != nil {
-		return jobsNoChanges, fmt.Errorf("failed to load job: %v", err)
-	}
-
-	jobsMu.Lock()
-	_, exists := jobs[jobName]
-	jobs[jobName] = job
-	jobsMu.Unlock()
-
-	if exists {
-		return jobsUpdated, nil
-	}
-
-	return jobsAddedNew, nil
+type Jobs struct {
+	byName map[string]Job
+	mu     *sync.RWMutex
 }
 
-type logWriter struct{}
-
-func (writer logWriter) Write(bytes []byte) (int, error) {
-	timestamp := time.Now().Format("2006-01-02 15:04:05 -0700")
-	return fmt.Printf("[%s] %s", timestamp, string(bytes))
-}
-
-func logJobPrintf(job, format string, v ...any) {
-	values := append([]any{job}, v...)
-	log.Printf("[%s] "+format, values...)
-}
-
-func cli() Config {
-	configRoot := pflag.StringP("config", "c", defaultConfigRoot, "path to config directory")
-	stateRoot := pflag.StringP("state", "s", defaultStateRoot, "path to state directory")
-
-	pflag.Usage = func() {
-		fmt.Fprintf(
-			os.Stderr,
-			"Usage: %s [options]\n\nOptions:\n",
-			filepath.Base(os.Args[0]),
-		)
-
-		pflag.PrintDefaults()
-	}
-
-	pflag.Parse()
-
-	return Config{
-		ConfigRoot: *configRoot,
-		StateRoot:  *stateRoot,
+func newJobs() Jobs {
+	return Jobs{
+		byName: make(map[string]Job),
+		mu:     &sync.RWMutex{},
 	}
 }
 
-func runJobs(jobs map[string]Job, jobsMu sync.RWMutex) {
+func (j Jobs) run() {
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 
@@ -126,10 +83,9 @@ func runJobs(jobs map[string]Job, jobsMu sync.RWMutex) {
 		now := time.Now()
 		thread := &starlark.Thread{Name: "scheduler"}
 
-		jobsMu.RLock()
-		defer jobsMu.RUnlock()
+		j.mu.RLock()
 
-		for name, job := range jobs {
+		for name, job := range j.byName {
 			if !job.Enabled {
 				continue
 			}
@@ -166,10 +122,40 @@ func runJobs(jobs map[string]Job, jobsMu sync.RWMutex) {
 				logJobPrintf(name, `"should_run" returned bad value: %v`, result)
 			}
 		}
+
+		j.mu.RUnlock()
 	}
 }
 
-func watchChanges(jobs map[string]Job, jobsMu sync.RWMutex, watcher *fsnotify.Watcher) {
+type updateJobsResult int
+
+const (
+	jobsNoChanges updateJobsResult = iota
+	jobsAddedNew
+	jobsUpdated
+)
+
+func (j Jobs) update(path string) (updateJobsResult, error) {
+	jobName := jobNameFromPath(path)
+
+	job, err := loadJob(path)
+	if err != nil {
+		return jobsNoChanges, fmt.Errorf("failed to load job: %v", err)
+	}
+
+	j.mu.Lock()
+	_, exists := j.byName[jobName]
+	j.byName[jobName] = job
+	j.mu.Unlock()
+
+	if exists {
+		return jobsUpdated, nil
+	}
+
+	return jobsAddedNew, nil
+}
+
+func (j Jobs) watchChanges(watcher *fsnotify.Watcher) {
 	debounced := debounce.New(100 * time.Millisecond)
 
 	for {
@@ -183,7 +169,7 @@ func watchChanges(jobs map[string]Job, jobsMu sync.RWMutex, watcher *fsnotify.Wa
 			jobName := jobNameFromPath(event.Name)
 
 			handleUpdate := func() {
-				res, err := updateJobs(jobs, jobsMu, event.Name)
+				res, err := j.update(event.Name)
 				if err != nil {
 					logJobPrintf(jobName, "Error updating job: %v", err)
 				}
@@ -208,9 +194,9 @@ func watchChanges(jobs map[string]Job, jobsMu sync.RWMutex, watcher *fsnotify.Wa
 				} else if event.Has(fsnotify.Write) {
 					debounced(handleUpdate)
 				} else if event.Has(fsnotify.Remove) {
-					jobsMu.Lock()
-					delete(jobs, jobName)
-					jobsMu.Unlock()
+					j.mu.Lock()
+					delete(j.byName, jobName)
+					j.mu.Unlock()
 
 					logJobPrintf(jobName, "Removed job")
 				}
@@ -226,9 +212,42 @@ func watchChanges(jobs map[string]Job, jobsMu sync.RWMutex, watcher *fsnotify.Wa
 	}
 }
 
+type logWriter struct{}
+
+func (writer logWriter) Write(bytes []byte) (int, error) {
+	timestamp := time.Now().Format("2006-01-02 15:04:05 -0700")
+	return fmt.Printf("[%s] %s", timestamp, string(bytes))
+}
+
+func logJobPrintf(job, format string, v ...any) {
+	values := append([]any{job}, v...)
+	log.Printf("[%s] "+format, values...)
+}
+
+func cli() Config {
+	configRoot := pflag.StringP("config", "c", defaultConfigRoot, "path to config directory")
+	stateRoot := pflag.StringP("state", "s", defaultStateRoot, "path to state directory")
+
+	pflag.Usage = func() {
+		fmt.Fprintf(
+			os.Stderr,
+			"Usage: %s [options]\n\nOptions:\n",
+			filepath.Base(os.Args[0]),
+		)
+
+		pflag.PrintDefaults()
+	}
+
+	pflag.Parse()
+
+	return Config{
+		ConfigRoot: *configRoot,
+		StateRoot:  *stateRoot,
+	}
+}
+
 func main() {
-	jobs := make(map[string]Job)
-	var jobsMu sync.RWMutex
+	jobs := newJobs()
 
 	log.SetFlags(0)
 	log.SetOutput(new(logWriter))
@@ -251,7 +270,7 @@ func main() {
 		}
 
 		if filepath.Base(path) == jobFileName {
-			_, err := updateJobs(jobs, jobsMu, path)
+			_, err := jobs.update(path)
 			if err != nil {
 				logJobPrintf(jobNameFromPath(path), "Error at startup: %v", err)
 			}
@@ -259,9 +278,12 @@ func main() {
 
 		return nil
 	})
+	if err != nil {
+		log.Fatalf("Error walking config dir: %v", err)
+	}
 
-	go runJobs(jobs, jobsMu)
-	go watchChanges(jobs, jobsMu, watcher)
+	go jobs.run()
+	go jobs.watchChanges(watcher)
 
 	// Block forever.
 	<-make(chan struct{})
